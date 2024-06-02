81a068da8eebe057b40ea4b9c00e6154aec2b645
unknown <jcgroup.vni@gmail.com>
2024-06-02 18:41:55 +0700

    fas

---
+++ adu.js
@@ -0,0 +1,26 @@
+const imgPosition = document.querySelectorAll(".aspect-ratio-169 img")
+const imgContainer = document.querySelector('.aspect-ratio-169')
+const dotItem = document.querySelectorAll(".dot")
+let imgNumber = imgPosition.length
+let index = 0
+imgPosition.forEach(function(image,index){
+    image.style.left = index*100 + "%"
+    dotItem[index].addEventListener("click",function(){
+        slider(index)
+    })
+})
+function imgSlide (){
+    index++;
+    if (index>=imgNumber){
+        index=0
+    }
+    slider(index)
+    
+}
+function slider(index) {
+    imgContainer.style.left = "-" + index*100 + "%"
+    const dotActive = document.querySelector('.active')
+    dotActive.classList.remove("active")
+    dotItem[index].classList.add("active")
+}
+setInterval(imgSlide,5000)
--- main.js
+++
@@ -1,4 +0,0 @@
-const imgPosition = document.querySelectorAll(".aspect-ratio-169 img")
-imgPosition.forEach(function(image,index){
-    image.style.left = index*100 + "%"
-})
