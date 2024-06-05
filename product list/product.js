488f131a6ceeae548ba146bdbe5d243709e939b7
unknown <jcgroup.vni@gmail.com>
2024-06-06 00:17:16 +0700

    fashion

--- product list/product.js
+++ product list/product.js
@@ -1,3 +1,13 @@
+const bigImg = document.querySelector(".product-content-left-big-img img")
+const smalImg = document.querySelectorAll(".product-content-left-small-img img")
+smalImg.forEach(function(imgItem,X){
+    imgItem.addEventListener("click",function(){
+        bigImg.src = imgItem.src
+    })
+})
+
+
+
 const chatlieu = document.querySelector(".chatlieu")
 const chitiet = document.querySelector(".chitiet")
 if(chatlieu){
@@ -5,10 +15,16 @@ if(chatlieu){
         document.querySelector(".product-content-right-bottom-content-chitiet").style.display = "none"
         document.querySelector(".product-content-right-bottom-content-chatlieu").style.display = "block"
     })
-}
+}       
 if(chitiet){
-    chatlieu.addEventListener("click",function(){
+    chitiet.addEventListener("click",function(){
         document.querySelector(".product-content-right-bottom-content-chitiet").style.display = "block"
         document.querySelector(".product-content-right-bottom-content-chatlieu").style.display = "none"
+    })
+}
+const butTon = document.querySelector(".product-content-right-bottom-top")
+if(butTon){ 
+    butTon.addEventListener("click",function(){
+        document.querySelector(".product-content-right-bottom-content-big").classList.toggle("activeB")
     })
 }
